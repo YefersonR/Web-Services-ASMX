3d14c30ebc1fb2f1b8ada78dc6507b225724f473
YefersonR <56859871+YefersonR@users.noreply.github.com>
2020-02-19 23:35:32 -0500

    Web Service ASMX

---
+++ WebService_Hora/MostrarHora/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MostrarHora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lhora = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lhora
+            // 
+            this.lhora.AutoSize = true;
+            this.lhora.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lhora.Location = new System.Drawing.Point(154, 71);
+            this.lhora.Name = "lhora";
+            this.lhora.Size = new System.Drawing.Size(92, 31);
+            this.lhora.TabIndex = 0;
+            this.lhora.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 205);
+            this.Controls.Add(this.lhora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lhora;
+    }
+}
+
---
+++ WebService_Hora/WebService_Hora/RetornarHora.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="RetornarHora.asmx.cs" Class="WebService_Hora.RetornarHora" %>
